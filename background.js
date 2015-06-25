22733fbc3ef1bfb7123cff8cabe573ac9b4c5902
Alex Emrie <emrieaj@gmail.com>
2015-06-24 20:24:18 -0600

    Added options for instagram, pinterest, reddit, youtube

--- background.js
+++ background.js
@@ -7,12 +7,20 @@ chrome.webNavigation.onCompleted.addListener(function(info) {
 
   chrome.storage.local.get(null, function(items) {
     if (items['active']) {
-      if (((currentTabURL.search("facebook")>-1) && items["facebook"]) || ((currentTabURL.search("twitter")>-1) && items["twitter"])) {
+      if (
+        ((currentTabURL.search("facebook")>-1) && items["facebook"]) ||
+        ((currentTabURL.search("instagram")>-1) && items["instagram"]) ||
+        ((currentTabURL.search("pinterest")>-1) && items["pinterest"]) ||
+        ((currentTabURL.search("instagram")>-1) && items["instagram"]) ||
+        ((currentTabURL.search("reddit")>-1) && items["reddit"]) ||
+        ((currentTabURL.search("twitter")>-1) && items["twitter"]) ||
+        ((currentTabURL.search("youtube")>-1) && items["youtube"])
+      ) {
         chrome.tabs.sendMessage(currentTabId, {"message": "visited monitored url"});
       }
     }
   })
-}, {url: [{urlEquals : 'https://www.facebook.com/'}, {urlEquals : 'https://twitter.com/'}]});
+}, {url: [{urlEquals : 'https://www.facebook.com/'}, {urlEquals : 'https://instagram.com/'}, {urlEquals : 'https://www.pinterest.com/'}, {urlEquals : 'http://www.reddit.com/'}, {urlEquals : 'https://twitter.com/'}, {hostContains : 'www.youtube.com'}]});
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
--- options.js
+++ options.js
@@ -2,13 +2,21 @@ function save_options() {
   var studyTime = document.getElementById('studyInterval').value;
   var breakTime = document.getElementById('breakInterval').value;
   var facebook = document.getElementById('facebook').checked;
+  var instagram = document.getElementById('instagram').checked;
+  var pinterest = document.getElementById('pinterest').checked;
+  var reddit = document.getElementById('reddit').checked;
   var twitter = document.getElementById('twitter').checked;
+  var youtube = document.getElementById('youtube').checked;
   var status = document.getElementById('status').checked;
   chrome.storage.local.set({
     "study": studyTime,
     "break": breakTime,
     "facebook": facebook,
-    "twitter": twitter    
+    "instagram": instagram,
+    "pinterest": pinterest,
+    "reddit": reddit,
+    "twitter": twitter,
+    "youtube": youtube
   }, function() {
     chrome.storage.local.remove('endTime');
     close_tab();
@@ -20,12 +28,20 @@ function restore_options() {
     "study": '30',
     "break": '10',
     "facebook": true,
-    "twitter": true
+    "instagram": true,
+    "pinterest": true,
+    "reddit": true,
+    "twitter": true,
+    "youtube": true
   }, function(items) {
     document.getElementById('studyInterval').value = items["study"];
     document.getElementById('breakInterval').value = items["break"];
     document.getElementById('facebook').checked = items["facebook"];
+    document.getElementById('instagram').checked = items["instagram"];
+    document.getElementById('pinterest').checked = items["pinterest"];
+    document.getElementById('reddit').checked = items["reddit"];
     document.getElementById('twitter').checked = items["twitter"];
+    document.getElementById('youtube').checked = items["youtube"];
   });
 }
 
